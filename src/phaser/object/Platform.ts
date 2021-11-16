08934a39efc4f5bc0fd5edcceefbb38728c1ad2a
Miscavel <miscavel@gmail.com>
2021-11-17 01:39:56 +0700

    Add moving platform

--- src/phaser/object/Platform.ts
+++ src/phaser/object/Platform.ts
@@ -2,6 +2,7 @@ export enum PLATFORM_TYPE {
   GROUND = 'ground',
   BASIC = 'basic',
   FRAGILE = 'fragile',
+  MOVING = 'moving',
 }
 
 export default class Platform extends Phaser.Physics.Arcade.Image {
@@ -9,6 +10,7 @@ export default class Platform extends Phaser.Physics.Arcade.Image {
     PLATFORM_TYPE.BASIC,
     PLATFORM_TYPE.BASIC,
     PLATFORM_TYPE.FRAGILE,
+    PLATFORM_TYPE.MOVING,
   ];
 
   constructor(
@@ -19,13 +21,18 @@ export default class Platform extends Phaser.Physics.Arcade.Image {
   ) {
     super(scene, x, y, 'platform');
 
-    this.adjustDisplayBasedOnType();
-
     scene.add.existing(this);
     scene.physics.add.existing(this);
+
+    this.adjustBasedOnType();
+
+    scene.events.on(Phaser.Scenes.Events.UPDATE, this.onUpdate, this);
+    scene.events.once(Phaser.Scenes.Events.SHUTDOWN, () => {
+      scene.events.off(Phaser.Scenes.Events.UPDATE, this.onUpdate, this);
+    });
   }
 
-  private adjustDisplayBasedOnType() {
+  private adjustBasedOnType() {
     const { width: screenWidth } = this.scene.cameras.main;
 
     const { platformType } = this;
@@ -33,18 +40,28 @@ export default class Platform extends Phaser.Physics.Arcade.Image {
       case PLATFORM_TYPE.GROUND: {
         this.setDisplaySize(screenWidth * 2, 16);
         this.setTint(0xffffff);
+        this.setVelocityX(0);
         break;
       }
 
       case PLATFORM_TYPE.BASIC: {
         this.setDisplaySize(72, 16);
         this.setTint(0xffffff);
+        this.setVelocityX(0);
         break;
       }
 
       case PLATFORM_TYPE.FRAGILE: {
         this.setDisplaySize(72, 16);
         this.setTint(0x00ff00);
+        this.setVelocityX(0);
+        break;
+      }
+
+      case PLATFORM_TYPE.MOVING: {
+        this.setDisplaySize(72, 16);
+        this.setTint(0x0000ff);
+        this.setVelocityX(Math.random() > 0.5 ? 100 : -100);
         break;
       }
     }
@@ -55,10 +72,32 @@ export default class Platform extends Phaser.Physics.Arcade.Image {
       this.randomTypePool[
         Math.floor(Math.random() * this.randomTypePool.length)
       ];
-    this.adjustDisplayBasedOnType();
+    this.adjustBasedOnType();
   }
 
   public isFragile() {
     return this.platformType === PLATFORM_TYPE.FRAGILE;
+  }
+
+  public isMoving() {
+    return this.platformType === PLATFORM_TYPE.MOVING;
+  }
+
+  private handleMovingPlatform() {
+    if (this.isMoving()) {
+      const { width: screenWidth } = this.scene.cameras.main;
+
+      if (this.x > screenWidth * 0.9 && this.body.velocity.x > 0) {
+        this.x = screenWidth * 0.9;
+        this.body.velocity.x *= -1;
+      } else if (this.x < screenWidth * 0.1 && this.body.velocity.x < 0) {
+        this.x = screenWidth * 0.1;
+        this.body.velocity.x *= -1;
+      }
+    }
+  }
+
+  private onUpdate() {
+    this.handleMovingPlatform();
   }
 }
