6330be45f242301e4043a1091c3b6109f7ea5340
babono <babono@hotmail.com>
2021-11-19 05:15:49 +0700

    4 files modified

--- index.tsx
+++ index.tsx
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 import PhaserWrapper from './src/react/PhaserWrapper';
+import Landing from './src/react/Landing';
 import './style.css';
 
 interface AppProps {}
 interface AppState {
   name: string;
+  isLanding: boolean;
+  isGameplay: boolean;
+  isResult: boolean;
 }
 
 class App extends Component<AppProps, AppState> {
@@ -13,11 +17,29 @@ class App extends Component<AppProps, AppState> {
     super(props);
     this.state = {
       name: 'React',
+      isLanding: true,
+      isGameplay: false,
+      isResult: false,
     };
   }
 
+  goToGameplay = () => {
+    this.setState({
+      isGameplay: true,
+      isLanding: false,
+      isResult: false,
+    });
+  };
+
   render() {
-    return <PhaserWrapper />;
+    const { isLanding, isGameplay, isResult } = this.state;
+
+    return (
+      <>
+        {isLanding && <Landing goToGameplay={this.goToGameplay} />}
+        {isGameplay && <PhaserWrapper />}
+      </>
+    );
   }
 }
 
--- src/react/Landing.tsx
+++ src/react/Landing.tsx
@@ -1,5 +1,93 @@
 import React, { useEffect } from 'react';
+import styled from 'styled-components';
 
-export default () => {
-  return <div>Yoooow</div>;
+interface LandingProps {
+  goToGameplay: () => void;
+}
+
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  position: relative;
+  background-color: #0B1728;
+  background-image: url('https://cdn.jsdelivr.net/gh/Miscavel/Garuda-Jump@master/public/assets/bg-hero-top.png');
+  background-repeat: no-repeat;
+  background-size: contain;
+`;
+
+const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  padding: 32px 64px;
+`;
+
+const Logo = styled.div`
+  width: 80vw;
+  height: 17vw;
+  background-size: contain;
+  background-position: center;
+  background-repeat: no-repeat;
+background-image: url('https://cdn.jsdelivr.net/gh/Miscavel/Garuda-Jump@master/public/assets/logo-garuda-jump.png');
+`;
+
+const HighscoreTitle = styled.div`
+  font-weight: bold;
+  font-size: 10vw;
+  color: #fff;
+`;
+
+const HighscoreText = styled.div`
+  font-weight: bold;
+  font-size: 15vw;
+  color: #fff;
+`;
+
+const HeroBot = styled.div`
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  background-image: url('https://cdn.jsdelivr.net/gh/Miscavel/Garuda-Jump@master/public/assets/bg-hero-bot.png');
+  width: 100%;
+  height: 20vh;
+  background-size: cover;
+  background-repeat: no-repeat;
+`;
+
+const Button = styled.div`
+  font-size: 12vw;
+  color: #fff;
+  background: #EA3953;
+  box-shadow: 0 8px #9A0319;
+  width: 100%;
+  text-align: center;
+  border-radius: 8px;
+`;
+
+const LogoSponsor = styled.div`
+  width: 80vw;
+  height: 17vw;
+  background-size: contain;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-image: url('https://cdn.jsdelivr.net/gh/Miscavel/Garuda-Jump@master/public/assets/logo-garuda-shopee.png');
+`;
+
+const Landing: React.FC<LandingProps> = ({ goToGameplay }) => {
+  return (
+    <Container>
+      <Content>
+        <Logo />
+        <HighscoreTitle>Highscore</HighscoreTitle>
+        <HighscoreText>130</HighscoreText>
+        <Button onClick={goToGameplay}>Play</Button>
+        <LogoSponsor />
+      </Content>
+      <HeroBot />
+    </Container>
+  );
 };
+
+export default Landing;
