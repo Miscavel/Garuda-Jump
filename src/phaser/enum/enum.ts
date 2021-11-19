6c9c176bcb90e7785a859b949c8ef7ca025b27f9
unknown <miscavel@gmail.com>
2021-11-19 16:05:45 +0700

    Add ASSET_KEY

---
+++ src/phaser/enum/enum.ts
@@ -0,0 +1,10 @@
+export enum ASSET_KEY {
+  GARUDA = 'garuda',
+  ATOM = 'atom',
+  GROUND = 'ground',
+  PLATFORM = 'platform',
+  CLOUD_PLATFORM = 'cloud_platform',
+  MOVING_PLATFORM = 'moving_platform',
+  TRAMPOLINE = 'trampoline',
+  TROPHY = 'trophy',
+}
