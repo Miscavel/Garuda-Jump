44b0c88f258e131f5f8120db04645ee58ad3a5da
unknown <miscavel@gmail.com>
2021-11-19 23:24:26 +0700

    Reset game for demo

--- src/phaser/object/Collectible.ts
+++ src/phaser/object/Collectible.ts
@@ -1,37 +1,5 @@
-import { ASSET_KEY } from "../enum/enum";
-import GameScene from "../scene/GameScene";
-import { getWeightedRandom } from "../util/random";
 import CustomContainer from "./CustomContainer";
 
 export default class Collectible extends CustomContainer {
-  private randomPool = {
-    [ASSET_KEY.ATOM]: 3,
-    [ASSET_KEY.TRAMPOLINE]: 1
-  };
-
-  constructor(
-    scene: GameScene,
-    x: number,
-    y: number,
-  ) {
-    super(scene, x, y);
-
-    scene.add.existing(this);
-    scene.physics.add.existing(this);
-
-    this.setTexture(ASSET_KEY.ATOM);
-  }
-
-  public randomize() {
-    const key = getWeightedRandom(this.randomPool);
-    this.setTexture(key);
-  }
-
-  public isAtom() {
-    return this.image.texture.key === ASSET_KEY.ATOM;
-  }
-
-  public isStar() {
-    return this.image.texture.key === ASSET_KEY.TRAMPOLINE;
-  }
+  
 }
--- src/phaser/object/Player.ts
+++ src/phaser/object/Player.ts
@@ -1,51 +1,5 @@
-import { ASSET_KEY } from "../enum/enum";
-import GameScene from "../scene/GameScene";
 import CustomContainer from "./CustomContainer";
 
 export default class Player extends CustomContainer {
-  constructor(scene: GameScene, x: number, y: number) {
-    super(scene, x, y);
-    
-    this.setTexture(ASSET_KEY.GARUDA);
-    this.body.setGravityY(1000);
-    this.body.setDragX(250);
-    this.body.setMaxVelocityX(1000);
-  }
-
-  private squishSprite() {
-    this.scene.tweens.add({
-      duration: 125,
-      targets: this.image,
-      scaleX: 0.9 * this.image.scaleX,
-      scaleY: 0.9 * this.image.scaleY,
-      yoyo: true,
-    });
-  }
-
-  public jump(strength: number) {
-    if (this.body.velocity.y >= 0) {
-      this.squishSprite();
-      this.body.setVelocityY(-600 * strength);
-      return true;
-    }
-    return false;
-  }
-
-  public tiltLeft() {
-    this.image.flipX = true;
-    this.setAccelerationX(-1000);
-  }
-
-  public tiltRight() {
-    this.image.flipX = false;
-    this.setAccelerationX(1000);
-  }
-
-  public setAccelerationX(accelX: number) {
-    this.body.setAccelerationX(accelX);
-  }
-
-  public resetAccelerationX() {
-    this.body.setAccelerationX(0);
-  }
+  
 }
