2396fc344d6bfa5fb699bad1e1e097ce8aa62d0b
unknown <miscavel@gmail.com>
2021-11-20 10:33:08 +0700

    Remove isGameplay

--- src/react/PhaserWrapper.tsx
+++ src/react/PhaserWrapper.tsx
@@ -3,7 +3,6 @@ import React, { useEffect } from 'react';
 import GameScene from '../phaser/scene/GameScene';
 
 interface PhaserWrapperProps {
-  isGameplay: () => boolean;
   getHighscore: () => number;
   updateCurrentScore: (score: number) => void;
   updateHighScore: (score: number) => void;
@@ -11,41 +10,38 @@ interface PhaserWrapperProps {
 }
 
 const PhaserWrapper: React.FC<PhaserWrapperProps> = ({
-  isGameplay,
   getHighscore,
   updateCurrentScore,
   updateHighScore,
   goToResult,
 }) => {
   useEffect(() => {
-    if (isGameplay()) {
-      const game = new Phaser.Game({
-        title: 'Garuda Jump',
-        scale: {
-          parent: 'game',
-          mode: Phaser.Scale.FIT,
-          autoCenter: Phaser.Scale.CENTER_BOTH,
-          width: 411,
-          height: 731,
+    const game = new Phaser.Game({
+      title: 'Garuda Jump',
+      scale: {
+        parent: 'game',
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH,
+        width: 411,
+        height: 731,
+      },
+      type: Phaser.AUTO,
+      physics: {
+        default: 'arcade',
+        arcade: {
+          debug: false,
         },
-        type: Phaser.AUTO,
-        physics: {
-          default: 'arcade',
-          arcade: {
-            debug: false,
-          },
-        },
-        scene: [GameScene],
-        backgroundColor: '#0B1728',
-      });
-      game.registry.set('highscore', getHighscore());
-      game.events.on('gameover', (score: number, highscore: number) => {
-        game.destroy(true);
-        goToResult();
-        updateCurrentScore(score);
-        updateHighScore(highscore);
-      });
-    }
+      },
+      scene: [GameScene],
+      backgroundColor: '#0B1728',
+    });
+    game.registry.set('highscore', getHighscore());
+    game.events.on('gameover', (score: number, highscore: number) => {
+      game.destroy(true);
+      goToResult();
+      updateCurrentScore(score);
+      updateHighScore(highscore);
+    });
   });
   return <div id="game" />;
 };
